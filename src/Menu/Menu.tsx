98d2082d8acd79d6cc9dbe4ec70f5ff9785205d8
Denner Lucio <denner.luciosl@gmail.com>
2023-10-17 01:09:18 -0300

    correção de layout de subcategorias com exibição

--- src/Menu/Menu.tsx
+++ src/Menu/Menu.tsx
@@ -16,19 +16,16 @@ interface Category {
   subcategories: Subcategory[];
 }
 
-
 export function Menu() {
   const [res, setRes] = useState<Category[]>([]);
-  
-  const totalCategorias: number = res.length;
 
+  const totalCategorias: number = res.slice(0, 5).length;
 
   const getRes = async () => {
     try {
       const response = await axios.get("https://localhost:5001/Category");
       const data = response.data;
       setRes(data);
-      
     } catch (error) {
       console.log(error);
     }
@@ -43,40 +40,76 @@ export function Menu() {
         <li className="cat_menu_principal">
           <img src={menu} alt="" id="menu_img" /> Todas as Categorias
           <ul className="filtro_oculto">
-            {res.slice(0, 5).map((resposta, index) => (
-              <div className={`container_mari ${index === totalCategorias -1 ? 'last-cat-div' : ''}`} key={resposta.id}>
-                <li className={`item_filtro ${index === totalCategorias -1 ? 'last-cat-li' : ''}`}>
+            {res.slice(0, totalCategorias).map((resposta, index) => (
+              <div
+                className={`container_mari ${
+                  index === totalCategorias - 1 ? "last-cat-div" : ""
+                }`}
+                key={resposta.id}
+              >
+                <li
+                  className={`item_filtro ${
+                    index === totalCategorias - 1 ? "last-cat-li" : ""
+                  }`}
+                >
                   {resposta.name}{" "}
                   <img src={seta} alt="" className="seta seta_sub" />
                 </li>
-                
+
                 <div className="subcategorias">
-                <ul className="ul_mari">
-                      <div className="div-sub-titulo" id="" key={resposta.id}> <h4> {resposta.name}   </h4> </div>
-                    {resposta.subcategories.slice(0, 5).map((subcategoria) => (
-                      <li className="li-sub-cat" key={subcategoria.subcategoryId}>
-                        {subcategoria.subcategoryName}{" "}
-                      </li>
-                    ))}
+                  <ul className="ul_mari">
+                  <div className="div-sub-titulo" id="" key={resposta.id}>
+                        {" "}
+                        <h4> {resposta.name} </h4>{" "}
+                      </div>
+                    <li className="li_mari">
+                      
+                        {resposta.subcategories
+                          .slice(0, 20)
+                          .map((subcategoria) => (
+                            <li
+                              className="li-sub-cat"
+                              key={subcategoria.subcategoryId}
+                            >
+                              {subcategoria.subcategoryName}{" "}
+                            </li>
+                          ))}
+                      
+                    </li>
                   </ul>
                 </div>
               </div>
             ))}
           </ul>
         </li>
-        {res.slice(0, 5).map((resposta) => (
+        {res.slice(0, totalCategorias).map((resposta) => (
           <li className="cat_menu_principal" key={resposta["id"]}>
             {resposta["name"]} <img src={seta} alt="" className="seta" />
-            <ul className="filtro_oculto">
-              {resposta.subcategories.slice(0, 5).map((subcategoria, index) => (
-
-                <div className= {`container_mari ${index === resposta.subcategories.length -1 ? 'last-sub-cat-div' : ''}`} key={subcategoria.subcategoryId }>
-                <li className = {`item_filtro ${index === resposta.subcategories.length -1 ? 'last-sub-cat-li' : ''}`} key={subcategoria.subcategoryId }>
-                  {subcategoria.subcategoryName}{" "}
-                  <img src={seta} alt="" className="seta" />
-                </li>
-                </div>
-              ))}
+            <ul className="filtro_oculto" id="sub-cat-overflow">
+              {resposta.subcategories
+                .slice(0, 10)
+                .map((subcategoria, index) => (
+                  <div
+                    className={`container_mari ${
+                      index === resposta.subcategories.length - 1
+                        ? "last-sub-cat-div"
+                        : ""
+                    }`}
+                    key={subcategoria.subcategoryId}
+                  >
+                    <li
+                      className={`item_filtro ${
+                        index === resposta.subcategories.length - 1
+                          ? "last-sub-cat-li"
+                          : ""
+                      }`}
+                      key={subcategoria.subcategoryId}
+                    >
+                      {subcategoria.subcategoryName}{" "}
+                      <img src={seta} alt="" className="seta" />
+                    </li>
+                  </div>
+                ))}
             </ul>
           </li>
         ))}
